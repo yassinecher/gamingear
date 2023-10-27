cdfab00c9530b58d9c9ec36950d781a600475897
yassinecher <yassinecherni9@gmail.com>
2023-10-27 01:38:31 +0100

    up

--- .history/app/(routes)/layout_20231012122717.tsx
+++ .history/app/(routes)/layout_20231012122717.tsx
@@ -2,7 +2,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Header from './_components/Header'
+import Header from '../_components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
---
+++ app/(routes)/page.tsx
@@ -0,0 +1,11 @@
+
+
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <>
+
+   <div>homezzzazza dsqsdadsqd dazzszadzdza zaddazd efzeeeeeeeeeeeeeeeeeeazds  salem</div></>
+  )
+}
